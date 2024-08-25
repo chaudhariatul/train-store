df92450fc550a8d80c76cafed8cc472cceb03963
Atul Chaudhari <chaudhariatul@gmail.com>
2024-08-25 15:40:58 -0700

    streamlit app ollama with gemma running locally

--- train_store/entrypoint.sh
+++ train_store/entrypoint.sh
@@ -12,10 +12,6 @@ term_handler() {
 
 trap 'kill ${!}; term_handler' SIGTERM
 /usr/bin/curl -s http://ollama_container:11434/api/pull -d '{"name":"gemma2:2b","stream":false}'
-/usr/bin/curl -s http://ollama_container:11434/api/pull -d '{"name":"gemma2:9b","stream":false}'
-/usr/bin/curl -s http://ollama_container:11434/api/pull -d '{"name":"gemma2:2b-instruct-q3_K_L","stream":false}'
-/usr/bin/curl -s http://ollama_container:11434/api/pull -d '{"name":"gemma2:2b-instruct-q8_0","stream":false}'
-/usr/bin/curl -s http://ollama_container:11434/api/pull -d '{"name":"llama3.1:8b-instruct-q4_K_M","stream":false}'
 /usr/local/bin/streamlit run /usr/train_store/Digital_Commander_DCC.py --server.port=8080 --server.address=0.0.0.0 --browser.gatherUsageStats=false &
 pid="$!"
 
