1367c883058947eb4b88d4965cfdcc1eb2891bcf
Atul Chaudhari <chaudhariatul@gmail.com>
2024-08-29 09:57:13 -0700

    Add reviews for Demo

--- setup_train_store_demo.sh
+++ setup_train_store_demo.sh
@@ -2,11 +2,5 @@
 
 git clone https://github.com/chaudhariatul/train-store
 cd train-store
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --no-cache-dir -U torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
-pip install --no-cache-dir -r train_store/requirements.txt
 docker-compose up -d
-cd train_store
-python add_reviews.py
-docker-compose down && docker-compose up -d
+docker exec -it train_store bash -c "cd /usr/train_store/ && python add_reviews.py"
