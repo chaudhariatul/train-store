e0b01d60101ad9e6e12348d189b0779f44793da9
Atul Chaudhari <chaudhariatul@gmail.com>
2024-08-28 19:27:19 -0700

    Setup script

--- setup_train_store_demo.sh
+++ setup_train_store_demo.sh
@@ -4,8 +4,9 @@ git clone https://github.com/chaudhariatul/train-store
 cd train-store
 python3 -m venv .venv
 source .venv/bin/activate
-python -m pip install --no-cache-dir -U torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
-pip install -r pip -q install --no-cache-dir -r train_store/requirements.txt
+pip install --no-cache-dir -U torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
+pip install --no-cache-dir -r train_store/requirements.txt
 docker-compose up -d
+cd train_store
 python add_reviews.py
 docker-compose down && docker-compose up -d
